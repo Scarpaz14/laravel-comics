80e62ee86b1f0231348cf6363ed10fd0803b57bf
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-07 16:21:37 +0200

    aggiunto header e cartella image

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,55 @@
-<header>
-    
+<div class="header-top container-fluid">
+    <div class="container">
+        <span>DC POWER VISA</span>
+        <span>ADDITIONAL SITES</span>
+    </div>
+</div>
+<div class="container-standard">
+<header class="header">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <img class="logo" src="{{asset('image/dc-logo.png')}}" alt="">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">CHARACTERS <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">COMICS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">MOVIES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">TV</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">GAMES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">COLLECTIBLES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">VIDEOS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">FANS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">NEWS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">SHOP</a>
+              </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
 </header>
+</div>
