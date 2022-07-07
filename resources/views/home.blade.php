bc0afd09926a58511f341f5ed964f07d1093dfd1
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-07 17:05:53 +0200

    fatta stampa delle card con foreach nella sezione main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,21 @@ DC Comics
 @endsection
 
 @section('page-content')
+<section>
+    <div class="container">
+        @foreach ($comics as $card)
+        <div class="singol-card">
+            <div class="card">
+                <img style="width: 70px" src="{{$card['thumb']}}" alt="thumb">
+                <div class="title">
+                    <small>{{$card['title']}}</small>
+                </div>
+            </div>
+        </div>
+        @endforeach
+        
+    </div>
+</section>
+
 
 @endsection
