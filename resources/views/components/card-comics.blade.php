b00a7ec005bc7b057cec7f9eb85aa7d42755f8ae
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-09 17:33:47 +0200

    sistemato stile

--- resources/views/components/card-comics.blade.php
+++ resources/views/components/card-comics.blade.php
@@ -2,8 +2,8 @@
     <div class="singol-card">
         <div class="card my-3">
             <img src="{{$image}}" alt="{{$title}}">
-            <div class="title">
-                <h5>{{$title}}</h5>
-            </div>
+        </div>
+        <div class="title">
+            <h5>{{$title}}</h5>
         </div>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,11 @@ DC Comics
 @section('page-content')
     <section id="main-section">
         <div class="container-main">
+            <button class="btn_current_series">CURRENT SERIES</button>
             <div class="row">
-                @foreach ($comics as $index => $card)
+                @foreach ($comics as $id => $card)
                 <div class="col">
-                    <a href="{{route('singolCard', ['id' => $index])}}">
+                    <a href="{{route('singolCard', ['id' => $id])}}">
                         <x-card-comics :image="$card['thumb']" :title="$card['title']"/>
                     </a>
                 </div>
